6970a603024f6b467a37183a9a5f70c4428e2315
Jay Welborn <jesse.welborn@gmail.com>
2018-07-30 22:05:54 +0200

    Update isPairwiseSorted.java

--- Chapter15/isPairwiseSorted.java
+++ Chapter15/isPairwiseSorted.java
@@ -1,7 +1,7 @@
 public boolean isPairwiseSorted() {
     for (int i = 1; i < this.size; i += 2) {
         if (elementData[i] < elementData[i - 1]) {
-            return false;
+            return false;
         }
     }
     return true;
